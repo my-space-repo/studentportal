a8075fe44921b62afee15a84b1fc2b379f371182
subr1990 <subramaniank@live.com>
2020-09-04 23:01:23 +1000

    Initial commit

---
+++ src/main/java/com/portal/student/models/ERole.java
@@ -0,0 +1,7 @@
+package com.portal.student.models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
---
+++ src/test/java/com/portal/student/StudentportalApplicationTests.java
@@ -0,0 +1,10 @@
+package com.portal.student;
+
+
+class StudentportalApplicationTests {
+
+
+    void contextLoads() {
+    }
+
+}
